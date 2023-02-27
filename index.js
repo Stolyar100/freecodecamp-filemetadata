cf693d385fc1d1316fb2c4b0860835ac141bb784
Oleh Marchuk <olehmarchukdev@gmail.com>
2023-02-27 15:52:40 +0200

    Finished

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import multer from 'multer'
+dotenv.config()
 
-var app = express();
+const app = express()
+const upload = multer()
 
-app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use(cors())
+app.use('/public', express.static(process.cwd() + '/public'))
 
 app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
-});
+  res.sendFile(process.cwd() + '/views/index.html')
+})
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  const { originalname, mimetype, size } = req.file
+  res.status(200).json({ name: originalname, type: mimetype, size: size })
+})
 
-
-
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)
-});
+})
